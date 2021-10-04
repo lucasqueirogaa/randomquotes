58d188827ff10c0822777d02ec3dde62faffc461
Lucas Queiroga <88801467+lucasqueirogaa@users.noreply.github.com>
2021-10-04 11:07:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ const quotesGenerator = function () {
   let quotes = {
     "- Albert Camus":
       "The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.",
+
     "- Lewis B. Smedes":
       "To forgive is to set a prisoner free and discover that the prisoner was you.",
+
+    "- Albert Einstein":
       "If you love someone, set them free. If they come back they're yours; if they don't they never were.",
+
     "- Denis Diderot":
       "Man will never be free until the last king is strangled with the entrails of the last priest.",
   };
